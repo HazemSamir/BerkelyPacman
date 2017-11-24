f140bdd7162fe4e22e9e4301400a32c4441aab95
hazem samir <hazem.s.ashmawy@gmail.com>
2017-11-24 13:07:07 +0200

    Solve first 4 questions

--- search/search.py
+++ search/search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -87,17 +87,72 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    actions = {}
+    frontier = util.Stack()
+    frontier.push(problem.getStartState())
+    explored = set()
+    actions[problem.getStartState()] = []
+    while not frontier.isEmpty():
+        state = frontier.pop()
+        explored.add(state)
+        if problem.isGoalState(state):
+            return actions[state]
+        for s in problem.getSuccessors(state):
+            if s[0] not in explored:
+                frontier.push(s[0])
+                actions[s[0]] = actions[state] + [s[1]]
+    return actions[state]
+
+
+
+
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    actions = {}
+    frontier = util.Queue()
+    frontier.push(problem.getStartState())
+    explored = set()
+    actions[problem.getStartState()] = []
+    explored.add(problem.getStartState())
+    while not frontier.isEmpty():
+        state = frontier.pop()
+        if problem.isGoalState(state):
+            return actions[state]
+        for s in problem.getSuccessors(state):
+            if s[0] not in explored:
+                frontier.push(s[0])
+                actions[s[0]] = actions[state] + [s[1]]
+                explored.add(s[0])
+    return state[1]
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    actions = {}
+    frontier = util.PriorityQueue()
+    frontier.push(problem.getStartState(), 0)
+    explored = set()
+    explored.add(problem.getStartState())
+    actions[problem.getStartState()] = ([],0)
+    while not frontier.isEmpty():
+        state = frontier.pop()
+        if problem.isGoalState(state):
+            return actions[state][0]
+        for s in problem.getSuccessors(state):
+            if s[0] not in explored and s[0] not in [x[2] for x in frontier.heap]:
+                priority = actions[state][1] + s[2]
+                frontier.push(s[0], priority)
+                actions[s[0]] = (actions[state][0] + [s[1]], priority)
+                explored.add(s[0])
+            elif s[0] in [x[2] for x in frontier.heap]:
+                frontier.update(s[0], actions[state][1] + s[2])
+                priority = actions[state][1] + s[2]
+                if actions[s[0]][1] > priority:
+                    actions[s[0]] = (actions[state][0] + [s[1]], priority)
+                explored.add(s[0])
+    return actions[state][0]
 
 def nullHeuristic(state, problem=None):
     """
@@ -109,7 +164,30 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    actions = {}
+    frontier = util.PriorityQueue()
+    h = heuristic(problem.getStartState(), problem)
+    frontier.push(problem.getStartState(), h)
+    explored = set()
+    explored.add(problem.getStartState())
+    actions[problem.getStartState()] = ([], 0)
+    while not frontier.isEmpty():
+        state = frontier.pop()
+        if problem.isGoalState(state):
+            return actions[state][0]
+        for s in problem.getSuccessors(state):
+            h = heuristic(s[0], problem)
+            cost = actions[state][1] + s[2]
+            if s[0] not in explored and s[0] not in [x[2] for x in frontier.heap]:
+                frontier.push(s[0], cost + h)
+                actions[s[0]] = (actions[state][0] + [s[1]], cost)
+                explored.add(s[0])
+            elif s[0] in [x[2] for x in frontier.heap]:
+                frontier.update(s[0], cost + h)
+                if actions[s[0]][1] > cost:
+                    actions[s[0]] = (actions[state][0] + [s[1]], cost)
+                explored.add(s[0])
+    return actions[state][0]
 
 
 # Abbreviations
